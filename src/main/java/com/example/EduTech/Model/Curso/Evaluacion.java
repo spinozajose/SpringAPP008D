392a709a82ed511553cbd19c9f2ef3ef90127fba
DanielAlcayaga <dan.alcayaga@duocuc.cl>
2025-05-22 16:07:30 -0400

    se creo entidad evaluacion

---
+++ src/main/java/com/example/EduTech/Model/Curso/Evaluacion.java
@@ -0,0 +1,25 @@
+package com.example.EduTech.Model.Curso;
+
+
+
+
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Entity
+public class Evaluacion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String materia;
+    private double nota;
+
