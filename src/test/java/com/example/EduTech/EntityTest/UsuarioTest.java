1137df685b305c2349b3af6b0de819ef4a0bb949
José Espinoza <josi.espinoza@duocuc.cl>
2025-07-03 00:30:53 -0400

    Prueba unitaria UsuarioModel

--- src/test/java/com/example/EduTech/EntityTest/UsuarioTest.java
+++ src/test/java/com/example/EduTech/EntityTest/UsuarioTest.java
@@ -1,13 +1,17 @@
-package com.example.EduTech;
+package com.example.EduTech.EntityTest;
 
 import com.example.EduTech.Model.Usuarios.Usuario;
 import com.example.EduTech.Repository.UsuarioRepository;
 import com.example.EduTech.Service.UsuarioService;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.test.context.ActiveProfiles;
+
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -15,11 +19,11 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class UsuarioTest {
 
     @Autowired
@@ -41,7 +45,7 @@ public class UsuarioTest {
     void checkNombreUsuarioTest() {
         Usuario usuario = usuarioRepository.findById(1).orElse(null);
         assertNotNull(usuario);
-        assertEquals("admin", usuario.getNombreUsuario());
+        assertEquals("ana_garcia", usuario.getNombreUsuario());
     }
 
     @Test
